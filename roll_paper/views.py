92b99996127c71710fbed2ed75da5c01b23851f7
minguno <minguno1128@gmail.com>
2022-05-01 00:47:28 +0900

    eunho/ heejin branch 변경사항 적용

--- roll_paper/views.py
+++ roll_paper/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    
-    return render(request, 'roll_paper/index.html')
-
 from django.http import HttpResponse
 from django.shortcuts import (render, redirect, get_object_or_404)
 from django.contrib import messages
@@ -18,6 +11,8 @@ from accounts.models import User
 from .forms import RollPaperForm
 
 # Create your views here.
+def index(request):
+    return render(request, 'roll_paper/index.html')
 
 def userlst(request):
     user_lst = User.objects.only('nickname')
@@ -29,13 +24,14 @@ def userlst(request):
 @require_http_methods(['GET', 'POST'])
 @login_required
 def write(request, user_pk):
-    # user = get_object_or_404(User, pk = user_pk)
-    
+   
     if request.method == 'POST':
         form = RollPaperForm(request.POST)
+        user = get_object_or_404(User, pk = user_pk)
         if form.is_valid():
             rollpaper = form.save(commit=False)
             rollpaper.author = request.user
+            rollpaper.user = user
             rollpaper.save()
 
             messages.add_message(request, messages.INFO, '편지가 성공적으로 작성되었습니다.')
@@ -64,7 +60,7 @@ def update(request, rollpaper_pk):
         form = RollPaperForm(request.POST, instance=rollpaper)
         if form.is_valid():
             form.save()
-            return redirect('roll_paper:detail', rollpaper.pk)
+            return redirect('rollpaper:detail', rollpaper.pk)
     else:
         form = RollPaperForm(instance=rollpaper)
 
@@ -83,4 +79,4 @@ def delete(request, rollpaper_pk):
     rollpaper = get_object_or_404(RollPaper, pk=rollpaper_pk)
     rollpaper.delete()
     messages.add_message(request, messages.ERROR, '편지가 성공적으로 삭제되었습니다!')
-    return redirect('roll_paper:user_lst')
+    return redirect('rollpaper:user_lst')
