4cd0a1f788ceb4e058986be85ec3a07cd0a1d794
minguno <minguno1128@gmail.com>
2022-05-18 19:38:04 +0900

    배포 테스트

---
+++ side_pjt/settings/local.py
@@ -0,0 +1,7 @@
+# config/settings/development.py
+
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ side_pjt/settings/production.py
@@ -0,0 +1,11 @@
+# config/settings/development.py
+
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+ # EC2 퍼블릭 주소
+ '13.125.225.154',
+ 'ddongddangs.com',
+]
