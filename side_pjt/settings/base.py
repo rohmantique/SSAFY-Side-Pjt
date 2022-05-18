6412bbc0facf6300bbc73fe1384f41a4efb59b8d
minguno <minguno1128@gmail.com>
2022-05-18 21:49:15 +0900

    eunho/ easteregg 완성

--- side_pjt/settings/base.py
+++ side_pjt/settings/base.py
@@ -120,7 +120,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
