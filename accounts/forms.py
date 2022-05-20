c02c65210f47ef9f1d8cd8d6a0f930de37413ed8
kimsuyeon <caelum0109@naver.com>
2022-05-20 13:41:08 +0900

    최종 오류 수정!!!

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,9 @@ from django.contrib.auth.forms import (
     UsernameField,
     PasswordChangeForm,
     SetPasswordForm,
+    PasswordResetForm
 )
+from django.utils.translation import gettext_lazy as _
 # from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import check_password
@@ -175,3 +177,13 @@ class CheckPasswordForm(forms.Form):
         if password:
             if not check_password(password, confirm_password):
                 self.add_error('password', '비밀번호가 일치하지 않습니다.')
+
+
+class CustomPasswordRestForm(PasswordResetForm):
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not User.objects.filter(email__iexact=email, is_active=True).exists():
+            msg = _("해당 이메일의 사용자가 존재하지 않습니다.")
+            self.add_error('email', msg)
+        return email
