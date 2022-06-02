0601204f7af4163743bae6e300233cef31721a58
minguno <minguno1128@gmail.com>
2022-06-02 14:56:24 +0900

    Delete unnecessary files

--- side_pjt/settings/base.py
+++ side_pjt/settings/base.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bzwq(s5n2&8hlmq0#=74z%l8w@0bv-qsh+#(+o4c(7t(ks6x)t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
